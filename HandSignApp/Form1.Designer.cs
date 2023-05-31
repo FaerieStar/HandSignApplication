f9ea9d4ee00454f9b04f08eb5316cf6ea0472b76
Stella Mariz Palen <stellamariz.palen@cit.edu>
2023-05-31 16:49:14 +0800

    Initial Commit

---
+++ HandSignApp/Form1.Designer.cs
@@ -0,0 +1,330 @@
+﻿namespace HandSignApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            create_model_btn = new Button();
+            groupBox1 = new GroupBox();
+            label8 = new Label();
+            olearn_rate_txt = new TextBox();
+            label5 = new Label();
+            ilearn_rate_txt = new TextBox();
+            label3 = new Label();
+            hneuron_size_txt = new TextBox();
+            groupBox2 = new GroupBox();
+            load_btn = new Button();
+            save_btn = new Button();
+            label6 = new Label();
+            train_btn = new Button();
+            epoch_num_txt = new TextBox();
+            label2 = new Label();
+            alphabet_box = new ComboBox();
+            groupBox3 = new GroupBox();
+            test_btn = new Button();
+            label7 = new Label();
+            input_image_btn = new Button();
+            output_show_txt = new TextBox();
+            image_display = new PictureBox();
+            input_image_dialog = new OpenFileDialog();
+            openFileDialog2 = new OpenFileDialog();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)image_display).BeginInit();
+            SuspendLayout();
+            // 
+            // create_model_btn
+            // 
+            create_model_btn.Location = new Point(6, 109);
+            create_model_btn.Name = "create_model_btn";
+            create_model_btn.Size = new Size(182, 30);
+            create_model_btn.TabIndex = 4;
+            create_model_btn.Text = "Create Model";
+            create_model_btn.UseVisualStyleBackColor = true;
+            create_model_btn.Click += create_model_btn_Click;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(label8);
+            groupBox1.Controls.Add(olearn_rate_txt);
+            groupBox1.Controls.Add(label5);
+            groupBox1.Controls.Add(ilearn_rate_txt);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(hneuron_size_txt);
+            groupBox1.Controls.Add(create_model_btn);
+            groupBox1.Location = new Point(12, 13);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(194, 148);
+            groupBox1.TabIndex = 1;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Settings";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(6, 84);
+            label8.Name = "label8";
+            label8.Size = new Size(110, 15);
+            label8.TabIndex = 12;
+            label8.Text = "Learning Rate(Out):";
+            // 
+            // olearn_rate_txt
+            // 
+            olearn_rate_txt.Location = new Point(127, 80);
+            olearn_rate_txt.Name = "olearn_rate_txt";
+            olearn_rate_txt.Size = new Size(61, 23);
+            olearn_rate_txt.TabIndex = 11;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(6, 55);
+            label5.Name = "label5";
+            label5.Size = new Size(100, 15);
+            label5.TabIndex = 10;
+            label5.Text = "Learning Rate(In):";
+            // 
+            // ilearn_rate_txt
+            // 
+            ilearn_rate_txt.Location = new Point(127, 51);
+            ilearn_rate_txt.Name = "ilearn_rate_txt";
+            ilearn_rate_txt.Size = new Size(61, 23);
+            ilearn_rate_txt.TabIndex = 3;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(6, 26);
+            label3.Name = "label3";
+            label3.Size = new Size(92, 15);
+            label3.TabIndex = 6;
+            label3.Text = "Hidden Neuron:";
+            // 
+            // hneuron_size_txt
+            // 
+            hneuron_size_txt.Location = new Point(127, 22);
+            hneuron_size_txt.Name = "hneuron_size_txt";
+            hneuron_size_txt.Size = new Size(61, 23);
+            hneuron_size_txt.TabIndex = 1;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(load_btn);
+            groupBox2.Controls.Add(save_btn);
+            groupBox2.Controls.Add(label6);
+            groupBox2.Controls.Add(train_btn);
+            groupBox2.Controls.Add(epoch_num_txt);
+            groupBox2.Controls.Add(label2);
+            groupBox2.Controls.Add(alphabet_box);
+            groupBox2.Location = new Point(12, 167);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(194, 191);
+            groupBox2.TabIndex = 2;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Dataset";
+            // 
+            // load_btn
+            // 
+            load_btn.Location = new Point(6, 152);
+            load_btn.Name = "load_btn";
+            load_btn.Size = new Size(182, 30);
+            load_btn.TabIndex = 14;
+            load_btn.Text = "Load";
+            load_btn.UseVisualStyleBackColor = true;
+            load_btn.Click += load_btn_Click;
+            // 
+            // save_btn
+            // 
+            save_btn.Location = new Point(6, 116);
+            save_btn.Name = "save_btn";
+            save_btn.Size = new Size(182, 30);
+            save_btn.TabIndex = 13;
+            save_btn.Text = "Save";
+            save_btn.UseVisualStyleBackColor = true;
+            save_btn.Click += save_btn_Click;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(6, 26);
+            label6.Name = "label6";
+            label6.Size = new Size(43, 15);
+            label6.TabIndex = 12;
+            label6.Text = "Epoch:";
+            // 
+            // train_btn
+            // 
+            train_btn.Location = new Point(6, 80);
+            train_btn.Name = "train_btn";
+            train_btn.Size = new Size(182, 30);
+            train_btn.TabIndex = 7;
+            train_btn.Text = "Train";
+            train_btn.UseVisualStyleBackColor = true;
+            train_btn.Click += train_btn_Click;
+            // 
+            // epoch_num_txt
+            // 
+            epoch_num_txt.Location = new Point(55, 22);
+            epoch_num_txt.Name = "epoch_num_txt";
+            epoch_num_txt.Size = new Size(133, 23);
+            epoch_num_txt.TabIndex = 5;
+            epoch_num_txt.Text = "5";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(6, 54);
+            label2.Name = "label2";
+            label2.Size = new Size(43, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Letter: ";
+            // 
+            // alphabet_box
+            // 
+            alphabet_box.FormattingEnabled = true;
+            alphabet_box.Items.AddRange(new object[] { "A", "B", "C", "D", "E", "F", "G", "H", "I", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y" });
+            alphabet_box.Location = new Point(55, 51);
+            alphabet_box.Name = "alphabet_box";
+            alphabet_box.Size = new Size(133, 23);
+            alphabet_box.TabIndex = 6;
+            // 
+            // groupBox3
+            // 
+            groupBox3.Controls.Add(test_btn);
+            groupBox3.Controls.Add(label7);
+            groupBox3.Controls.Add(input_image_btn);
+            groupBox3.Controls.Add(output_show_txt);
+            groupBox3.Controls.Add(image_display);
+            groupBox3.Location = new Point(230, 13);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(227, 345);
+            groupBox3.TabIndex = 3;
+            groupBox3.TabStop = false;
+            groupBox3.Text = "Testing";
+            // 
+            // test_btn
+            // 
+            test_btn.Location = new Point(87, 306);
+            test_btn.Name = "test_btn";
+            test_btn.Size = new Size(75, 30);
+            test_btn.TabIndex = 9;
+            test_btn.Text = "Test";
+            test_btn.UseVisualStyleBackColor = true;
+            test_btn.Click += test_btn_Click;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(38, 210);
+            label7.Name = "label7";
+            label7.Size = new Size(40, 15);
+            label7.TabIndex = 14;
+            label7.Text = "Letter:";
+            // 
+            // input_image_btn
+            // 
+            input_image_btn.Location = new Point(6, 306);
+            input_image_btn.Name = "input_image_btn";
+            input_image_btn.Size = new Size(75, 30);
+            input_image_btn.TabIndex = 8;
+            input_image_btn.Text = "Input";
+            input_image_btn.UseVisualStyleBackColor = true;
+            input_image_btn.Click += input_image_btn_Click;
+            // 
+            // output_show_txt
+            // 
+            output_show_txt.Location = new Point(87, 206);
+            output_show_txt.Name = "output_show_txt";
+            output_show_txt.ReadOnly = true;
+            output_show_txt.Size = new Size(102, 23);
+            output_show_txt.TabIndex = 10;
+            // 
+            // image_display
+            // 
+            image_display.Location = new Point(39, 22);
+            image_display.Name = "image_display";
+            image_display.Size = new Size(150, 150);
+            image_display.TabIndex = 0;
+            image_display.TabStop = false;
+            // 
+            // input_image_dialog
+            // 
+            input_image_dialog.Filter = "Image files|*.png|*.jpeg|*.jpg";
+            // 
+            // openFileDialog2
+            // 
+            openFileDialog2.FileName = "openFileDialog2";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(474, 370);
+            Controls.Add(groupBox3);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Name = "Form1";
+            Text = "  ";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            groupBox3.ResumeLayout(false);
+            groupBox3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)image_display).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button create_model_btn;
+        private GroupBox groupBox1;
+        private Label label5;
+        private TextBox ilearn_rate_txt;
+        private Label label3;
+        private TextBox hneuron_size_txt;
+        private GroupBox groupBox2;
+        private Label label6;
+        private Button train_btn;
+        private TextBox epoch_num_txt;
+        private Label label2;
+        private ComboBox alphabet_box;
+        private GroupBox groupBox3;
+        private Label label7;
+        private Button input_image_btn;
+        private TextBox output_show_txt;
+        private PictureBox image_display;
+        private Button test_btn;
+        private OpenFileDialog input_image_dialog;
+        private OpenFileDialog openFileDialog2;
+        private Label label8;
+        private TextBox olearn_rate_txt;
+        private Button load_btn;
+        private Button save_btn;
+    }
+}
---
+++ HandSignApp/Form1.cs
@@ -0,0 +1,190 @@
+using Backprop;
+using System.Diagnostics;
+using System.Drawing;
+
+namespace HandSignApp
+{
+    public partial class Form1 : Form
+    {
+        private readonly string project_path = Directory.GetParent(Directory.GetParent(Directory.GetParent(Directory.GetCurrentDirectory()).FullName).FullName).FullName;
+        private NeuralNet net;
+        private string dataset_directory = "D:\\Projects\\Dataset\\Train";
+        private string weights_file;
+        private string bias_file;
+        private string input_file;
+
+        public Form1()
+        {
+            InitializeComponent();
+            weights_file = project_path + "\\weights.csv";
+            bias_file = project_path + "\\bias.csv";
+            //Debug.WriteLine(project_path + "\\Dataset\\Train\\train.csv");
+        }
+
+        private void create_model_btn_Click(object sender, EventArgs e)
+        {
+            int isize = 784;
+            int hsize = Convert.ToInt32(hneuron_size_txt.Text);
+            int osize = 24;
+            double lrpin = Convert.ToDouble(ilearn_rate_txt.Text);
+            double lrpout = Convert.ToDouble(olearn_rate_txt.Text);
+            net = new NeuralNet(isize, hsize, osize);
+            /*if (lrpin.Equals("") && lrpout.Equals(""))
+                net = new NeuralNetwork(isize, hsize, osize);
+
+            net = new NeuralNetwork(isize, hsize, osize, lrpin, lrpout);*/
+
+            Debug.WriteLine(project_path);
+        }
+
+        private void train_btn_Click(object sender, EventArgs e)
+        {
+            char[] letter = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y' };
+            int epoch = Convert.ToInt32(epoch_num_txt.Text);
+
+            // CONVERT TRAINING DATA TO CSV
+            /*using (StreamWriter csvWriter = new StreamWriter(dataset_directory + "/0Train/train.csv"))
+            {
+                int num = 1;
+                int len_test = 1;
+                int len = Directory.GetFiles(dataset_directory).Length;
+                foreach (string file in Directory.GetFiles(dataset_directory))
+                {
+                    string[] filename_split = file.Split('\\');
+                    string name_only = filename_split[filename_split.Length - 1].Split('.')[0];
+                    Debug.WriteLine(num);
+                    Debug.WriteLine("File: " + name_only + ", ");
+                    for (int i = 0; i < letter.Length; i++)
+                    {
+                        if (name_only.Contains(letter[i]))
+                        {
+                            Debug.WriteLine("OUTPUT: " + i);
+                            csvWriter.Write(i + ",");
+                        }
+                    }
+
+                    Bitmap bm = new Bitmap(file);
+                    for (int x = 0; x < bm.Height; x++)
+                    {
+                        for (int y = 0; y < bm.Width; y++)
+                        {
+                            int grayscale_intensity = bm.GetPixel(x, y).R;
+                            csvWriter.Write(grayscale_intensity);
+
+                            if ((y + (x * 28)) < 783)
+                                csvWriter.Write(",");
+                        }
+                    }
+                    if (len_test < (len))
+                        csvWriter.WriteLine();
+                    len_test++;
+                    num++;
+                }
+            }*/
+
+            for (int x = 0; x < epoch; x++)
+            {
+                Debug.WriteLine("Epoch: " + (x + 1));
+                using (StreamReader csvReader = new StreamReader(project_path + "\\Dataset\\train.csv"))
+                //using (StreamReader csvReader = new StreamReader(dataset_directory + "/0Train/train.csv"))
+                {
+                    int len = 1;
+                    while (!csvReader.EndOfStream)
+                    {
+                        Debug.WriteLine("Line: " + len);
+                        string[] values = csvReader.ReadLine().Split(',');
+                        int oid = Convert.ToInt32(values[0]);
+
+                        for (int i = 1; i < values.Length; i++)
+                            net.setInputs((i - 1), Convert.ToDouble(values[i]));
+
+                        for (int i = 0; i < letter.Length; i++)
+                        {
+                            if (oid == i)
+                            {
+                                Debug.WriteLine("Letter: " + letter[i]);
+                                net.setDesiredOutput(i, 1);
+                            }
+                            else
+                                net.setDesiredOutput(i, 0);
+                        }
+                        //net.learn();
+                        len++;
+                    }
+                }
+                net.learn();
+            }
+        }
+
+        private void input_image_btn_Click(object sender, EventArgs e)
+        {
+
+            if (input_image_dialog.ShowDialog() == DialogResult.OK)
+            {
+                this.input_file = input_image_dialog.FileName;
+
+                Bitmap bm = new Bitmap(new Bitmap(this.input_file), new Size(150, 150));
+                bm.SetResolution(96, 96);
+
+                Image rImage = Image.FromHbitmap(bm.GetHbitmap());
+
+                image_display.Image = rImage;
+            }
+        }
+
+        private void test_btn_Click(object sender, EventArgs e)
+        {
+            Debug.WriteLine(this.input_file);
+            Bitmap bm = new Bitmap(this.input_file);
+            for (int x = 0; x < bm.Height; x++)
+            {
+                for (int y = 0; y < bm.Width; y++)
+                {
+                    int grayscale_intensity = bm.GetPixel(x, y).R;
+                    Debug.WriteLine((y + (x * 28)) + ": " + grayscale_intensity);
+                    net.setInputs(y + (x * 28), grayscale_intensity);
+                }
+            }
+            net.run();
+
+            string result = "";
+            double max = 0.0;
+            double curr = 0.0;
+            int indexmax = 0;
+            for (int i = 0; i < 24; i++)
+            {
+                curr = net.getOuputData(i);
+                result += "Output[" + (i + 1) + "]: " + curr + "\n";
+                if (curr > max)
+                {
+                    max = curr;
+                    indexmax = i + 1;
+                }
+            }
+            //result += "Output[" + (i + 1) + "]: " + net.getOuputData(i) + "\n";
+            if (indexmax < 10)
+            {
+                output_show_txt.Text = Convert.ToString(Convert.ToChar(64 + indexmax));
+            }
+            else
+            {
+                output_show_txt.Text = Convert.ToString(Convert.ToChar(65 + indexmax));
+            }
+            MessageBox.Show(result);
+
+        }
+
+        private void save_btn_Click(object sender, EventArgs e)
+        {
+            net.saveWeights(weights_file);
+            //net.Save(weights_file, bias_file);
+        }
+
+        private void load_btn_Click(object sender, EventArgs e)
+        {
+            net.loadWeights(weights_file);
+            //net.Load(weights_file, bias_file);
+        }
+
+    }
+}
